544cfa963eaf8b028114f6e6c67550f4ef8d0d79
Davin <85967571+dav1n9@users.noreply.github.com>
2024-03-25 23:05:34 +0900

    ProjectSkillRepository 추가 생성

---
+++ src/main/java/com/davin/portfolio/domain/repository/ProjectSkillRepository.java
@@ -0,0 +1,7 @@
+package com.davin.portfolio.domain.repository;
+
+import com.davin.portfolio.domain.entity.ProjectSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectSkillRepository extends JpaRepository<ProjectSkill, Long> {
+}
