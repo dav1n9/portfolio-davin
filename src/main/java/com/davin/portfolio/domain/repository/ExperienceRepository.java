db165abbbbd1b22e14d9a82b5142e1af6142e981
Davin <85967571+dav1n9@users.noreply.github.com>
2024-03-30 20:39:02 +0900

    perf: 리포지토리 성능 개선
    - 직접 JPA 쿼리를 정의 (fetch join)
    - Batch Fetch Size 설정

--- src/main/java/com/davin/portfolio/domain/repository/ExperienceRepository.java
+++ src/main/java/com/davin/portfolio/domain/repository/ExperienceRepository.java
@@ -2,9 +2,17 @@ package com.davin.portfolio.domain.repository;
 
 import com.davin.portfolio.domain.entity.Experience;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExperienceRepository extends JpaRepository<Experience, Long> {
-    List<Experience> findAllByIsActive(Boolean isActive);
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
+    List<Experience> findAllByIsActive(@Param("isActive") boolean isActive);
+
+    @Override
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
+    Optional<Experience> findById(@Param("id") Long id);
 }
--- src/main/java/com/davin/portfolio/domain/repository/ProjectRepository.java
+++ src/main/java/com/davin/portfolio/domain/repository/ProjectRepository.java
@@ -2,9 +2,17 @@ package com.davin.portfolio.domain.repository;
 
 import com.davin.portfolio.domain.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    List<Project> findAllByIsActive(Boolean isActive);
+    @Query("select p from Project p left join fetch p.skills s left join fetch s.skill where p.isActive = :isActive")
+    List<Project> findAllByIsActive(@Param("isActive") Boolean isActive);
+
+    @Override
+    @Query("select p from Project p left join fetch p.details where p.id = :id")
+    Optional<Project> findById(@Param("id") Long id);
 }
