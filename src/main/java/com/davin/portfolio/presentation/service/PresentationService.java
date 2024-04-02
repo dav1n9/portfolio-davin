16c21f8e09d600489593b1d0fad1a61fd2285595
Davin <85967571+dav1n9@users.noreply.github.com>
2024-04-02 21:29:08 +0900

    feat: 서비스 개발

--- src/main/java/com/davin/portfolio/presentation/service/PresentationService.java
+++ src/main/java/com/davin/portfolio/presentation/service/PresentationService.java
@@ -1,7 +1,45 @@
 package com.davin.portfolio.presentation.service;
 
+import com.davin.portfolio.domain.entity.*;
+import com.davin.portfolio.presentation.dto.IntroductionDTO;
+import com.davin.portfolio.presentation.dto.LinkDTO;
+import com.davin.portfolio.presentation.dto.ProjectDTO;
+import com.davin.portfolio.presentation.dto.ResumeDTO;
+import com.davin.portfolio.presentation.repository.PresentationRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class PresentationService {
+    private final PresentationRepository presentationRepository;
+
+    @Transactional(readOnly = true)
+    public List<IntroductionDTO> getIntroductions() {
+        List<Introduction> introductions = presentationRepository.getActiveIntroductions();
+        return introductions.stream().map(IntroductionDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<LinkDTO> getLinks() {
+        List<Link> links = presentationRepository.getActiveLinks();
+        return links.stream().map(LinkDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ResumeDTO getResume() {
+        List<Experience> experiences = presentationRepository.getActiveExperiences();
+        List<Achievement> achievements = presentationRepository.getActiveAchievements();
+        List<Skill> skills = presentationRepository.getActiveSkills();
+        return new ResumeDTO(experiences, achievements, skills);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProjectDTO> getProjects() {
+        List<Project> projects = presentationRepository.getActiveProjects();
+        return projects.stream().map(ProjectDTO::new).toList();
+    }
 }
