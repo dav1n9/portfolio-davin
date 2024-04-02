b49b4a21d295d99fb2c0b17252be37828a420bbc
Davin <85967571+dav1n9@users.noreply.github.com>
2024-04-02 21:01:47 +0900

    feat: 리포지토리 개발

--- src/main/java/com/davin/portfolio/presentation/repository/PresentationRepository.java
+++ src/main/java/com/davin/portfolio/presentation/repository/PresentationRepository.java
@@ -1,7 +1,38 @@
 package com.davin.portfolio.presentation.repository;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.davin.portfolio.domain.entity.*;
+import com.davin.portfolio.domain.repository.*;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
 
-@RequestMapping
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
 public class PresentationRepository {
+    private AchievementRepository achievementRepository;
+    private ExperienceRepository experienceRepository;
+    private IntroductionRepository introductionRepository;
+    private LinkRepository linkRepository;
+    private SkillRepository skillRepository;
+    private ProjectRepository projectRepository;
+
+    public List<Achievement> getActiveAchievements() {
+        return achievementRepository.findAllByIsActive(true);
+    }
+    public List<Experience> getActiveExperiences() {
+        return experienceRepository.findAllByIsActive(true);
+    }
+    public List<Introduction> getActiveIntroductions() {
+        return introductionRepository.findAllByIsActive(true);
+    }
+    public List<Link> getActiveLinks() {
+        return linkRepository.findAllByIsActive(true);
+    }
+    public List<Skill> getActiveSkills() {
+        return skillRepository.findAllByIsActive(true);
+    }
+    public List<Project> getActiveProjects() {
+        return projectRepository.findAllByIsActive(true);
+    }
 }
