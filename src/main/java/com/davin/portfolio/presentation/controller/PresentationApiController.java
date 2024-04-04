dfc30b9c1a52ef8102d280f53672a8c72ccfc579
Davin <85967571+dav1n9@users.noreply.github.com>
2024-04-04 20:32:29 +0900

    feat: 컨트롤러 개발

--- src/main/java/com/davin/portfolio/presentation/controller/PresentationApiController.java
+++ src/main/java/com/davin/portfolio/presentation/controller/PresentationApiController.java
@@ -1,10 +1,40 @@
 package com.davin.portfolio.presentation.controller;
 
+import com.davin.portfolio.presentation.dto.IntroductionDTO;
+import com.davin.portfolio.presentation.dto.LinkDTO;
+import com.davin.portfolio.presentation.dto.ProjectDTO;
+import com.davin.portfolio.presentation.dto.ResumeDTO;
+import com.davin.portfolio.presentation.service.PresentationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class PresentationApiController {
+    private final PresentationService presentationService;
 
+    @GetMapping("/v1/introductions")
+    public List<IntroductionDTO> getIntroductions() {
+        return presentationService.getIntroductions();
+    }
+
+    @GetMapping("/v1/links")
+    public List<LinkDTO> getLinks() {
+        return presentationService.getLinks();
+    }
+
+    @GetMapping("/v1/resume")
+    public ResumeDTO getResume() {
+        return presentationService.getResume();
+    }
+
+    @GetMapping("/v1/projects")
+    public List<ProjectDTO> getProjects() {
+        return presentationService.getProjects();
+    }
 }
--- src/main/java/com/davin/portfolio/presentation/controller/PresentationViewController.java
+++ src/main/java/com/davin/portfolio/presentation/controller/PresentationViewController.java
@@ -1,8 +1,48 @@
 package com.davin.portfolio.presentation.controller;
 
+import com.davin.portfolio.domain.constant.SkillType;
+import com.davin.portfolio.presentation.dto.IntroductionDTO;
+import com.davin.portfolio.presentation.dto.LinkDTO;
+import com.davin.portfolio.presentation.dto.ProjectDTO;
+import com.davin.portfolio.presentation.dto.ResumeDTO;
+import com.davin.portfolio.presentation.service.PresentationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class PresentationViewController {
-    
+    private final PresentationService presentationService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<IntroductionDTO> introductions = presentationService.getIntroductions();
+        model.addAttribute("introductions", introductions);
+
+        List<LinkDTO> links = presentationService.getLinks();
+        model.addAttribute("links", links);
+
+        return "presentation/index";
+    }
+
+    @GetMapping("/resume")
+    public String resume(Model model) {
+        ResumeDTO resume = presentationService.getResume();
+        model.addAttribute("resume", resume);
+        model.addAttribute("skillTypes", SkillType.values());
+
+        return "presentation/resume";
+    }
+
+    @GetMapping("/projects")
+    public String projects(Model model) {
+        List<ProjectDTO> projects = presentationService.getProjects();
+        model.addAttribute("projects", projects);
+
+        return "presentation/projects";
+    }
 }
